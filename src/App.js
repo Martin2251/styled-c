06866842bc65aa401798fc7df23e84d9e9759a88
martin lowe <martinlowe673@gmail.com>
2021-08-12 07:25:39 +0100

    added props to the styled components

--- src/App.js
+++ src/App.js
@@ -15,19 +15,21 @@ function App() {
       </Button>
 
       <TomatoButton>hello</TomatoButton>
+      <Button primary>Oi</Button>
     </div>
   );
 }
 
 const Button = styled.a`
-  display: inline-block;
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
   border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: blue;
-  color: black;
-  border: 2px black;
   text-decoration: none;
 `;
 
